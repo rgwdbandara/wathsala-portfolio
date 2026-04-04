afe3ceac7d125e087c3f51a58dcbca3801e6f262
rgwdbandara <rgwdbandara@nsbm.students.ac.lk>
2026-04-04 17:42:36 +0530

    Initialize portfolio project with React Vite and Setup Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white text-3xl">
+      Wathsala Portfolio 🚀
+    </div>
+  );
+}
+
+export default App;

0dd90b115a5f0c1db17247d629c384b0184325a9
rgwdbandara <rgwdbandara@nsbm.students.ac.lk>
2026-06-12 13:46:26 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import { motion } from "framer-motion";
-
-<motion.div
-  initial={{ opacity: 0, y: 40 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 0.8 }}
->
-  <App />
-</motion.div>
-
 
 function Hero() {
   return (
